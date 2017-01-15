1db7ed1d8791a558c056f80b3c5afe9159de3d64
pmgexpo17 <pmg7670@gmail.com>
2017-01-15 19:13:47 +1000

    JMS Container (ClientPeer) Base Package

---
+++ jmsClientPeer/src/main/java/org/pmg/jms/SessionAgent.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2016 Peter A McGill
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. 
+**/
+package org.pmg.jms.genconnect;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.Topic;
+import org.pmg.jms.genbase.LifeCycle;
+
+/**
+ * Defines session agent works
+ * @author Peter A McGill
+ */
+public interface SessionAgent extends LifeCycle {
+
+    public Queue createQueue(String queueName) throws JMSException;
+    public Topic createTopic(String topicName) throws JMSException;
+    public Destination createDestination(String destName) throws JMSException;
+    public Session getSession();
+    public String getTransportName();
+}
